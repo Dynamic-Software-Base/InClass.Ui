53959a850a07f4f32b95655136a6ee64319b8f8e
EmperorXSama <5alidghoddane@protonmail.com>
2026-02-21 14:46:32 +0000

    fix logout and redirectionsda

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  bffBaseUrl: 'https://inclass-bff-bqg5g9d6caa9g4es.westeurope-01.azurewebsites.net'
+  bffBaseUrl: ''  // empty string = same origin
 };
